45b0c73b59d98e0d4de67423ba66bc8cd4a7cfaf
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-29 14:50:42 +0200

    Create SharedLayout.jsx

---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,31 @@
+//========== components ==========
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Container } from 'components/Container/Container';
+
+//========== styles ==========
+import { SearchMovieApp, HeaderNav, PagesList, Item, HomeLink, MoviesLink } from './SharedLayout.styled';
+
+const SharedLayout = () => {
+    return <SearchMovieApp>
+        <HeaderNav>
+            <Container>
+                <nav>
+                    <PagesList>
+                        <Item>
+                            <HomeLink to="/">Home</HomeLink>
+                        </Item>
+                        <Item>
+                            <MoviesLink to="/movies">Movies</MoviesLink>
+                        </Item>
+                    </PagesList>
+                </nav>
+            </Container>
+        </HeaderNav>
+        <Suspense >
+            <Outlet />
+        </Suspense>
+    </SearchMovieApp>;
+};
+
+export { SharedLayout };
