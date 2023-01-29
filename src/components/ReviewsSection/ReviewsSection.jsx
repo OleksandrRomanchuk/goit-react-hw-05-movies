a24f89f1c25f9f80c4517865e77c96aaf09da67d
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-29 14:50:25 +0200

    Create ReviewsSection.jsx

---
+++ src/components/ReviewsSection/ReviewsSection.jsx
@@ -0,0 +1,50 @@
+import MovieAPI from "API/API";
+
+//========== hooks ==========
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+//========== styles ==========
+import { Wrapper, Title, Message, List, Item, Author, Devider, AuthorSpan, Content } from "./ReviewsSection.styled";
+
+const ReviewsSection = () => {
+    const [reviews, setReviews] = useState(null);
+    const { id } = useParams();
+
+    useEffect(() => {
+        MovieAPI.getMovieReviews(id).then(reviews => {
+            const info = reviews.reduce((acc, review) => {
+                const rev = {
+                    id: review.id,
+                    name: review.author,
+                    content: review.content,
+                };
+                return [...acc, rev]
+            }, [])
+
+            setReviews(info);
+        });
+    }, [id]);
+
+    if (!reviews) return;
+
+    const reviewsCount = Boolean(reviews.length);
+
+    return <Wrapper>
+        <Title>Rewiews</Title>
+        {!reviewsCount && <Message>This movie has no reviews yet.</Message>}
+        {reviewsCount && <>
+            <Message>
+                This movie has {reviews.length} reviews.
+            </Message>
+            <List>
+                {reviews.map(({ id, name, content }) => <Item key={id}>
+                    <Author>Author<Devider>I</Devider><AuthorSpan>{name}</AuthorSpan></Author>
+                    <Content>{content}</Content>
+                </Item>)}
+            </List>
+        </>}
+    </Wrapper>;
+};
+
+export default ReviewsSection;
