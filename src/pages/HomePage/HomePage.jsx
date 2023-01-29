f793e34746930ef0838deb7356172d5e76d9a5be
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-29 14:49:47 +0200

    Create HomePage.jsx

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,42 @@
+import MovieAPI from "API/API";
+
+//========== hooks ==========
+import { useState, useEffect } from "react";
+
+//========== components ==========
+import { Container } from 'components/Container/Container';
+import { MoviesList } from "components/MoviesList/MoviesList";
+import placeholder from 'images/placeholder.jpg'
+
+const HomePage = () => {
+    const [movies, setMovies] = useState(null);
+
+    useEffect(() => {
+        MovieAPI.getTrendingMovies('week').then((movies => {
+            const info = movies.reduce((acc, movie) => {
+                const poster = movie.poster_path ? `https://image.tmdb.org/t/p/original/${movie.poster_path}` : placeholder;
+                const film = {
+                    id: movie.id,
+                    title: movie.title,
+                    release_date: movie.release_date,
+                    poster_path: poster,
+                    vote_average: movie.vote_average
+                };
+                return [...acc, film]
+            }, []);
+
+            setMovies(info);
+        }));
+    }, []);
+
+    if (!movies) return;
+
+    return <main>
+        <Container>
+            <MoviesList
+                moviesInfo={movies} />
+        </Container>
+    </main>;
+};
+
+export default HomePage;
