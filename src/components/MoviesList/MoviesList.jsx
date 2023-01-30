553551a22ad4d13679d4d3ed72eca86dfe8986d6
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-30 11:57:23 +0200

    fix releaseDate === undefind

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -7,41 +7,65 @@ import { useLocation } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 
 //========== styles ==========
-import { List, Item, MoviePoster, Wrapper, Title, InfoWrapper, Info, DateInfo, Divider } from './MoviesList.styled';
+import {
+	List,
+	Item,
+	MoviePoster,
+	Wrapper,
+	Title,
+	InfoWrapper,
+	Info,
+	DateInfo,
+	Divider,
+} from './MoviesList.styled';
 
 const MoviesList = ({ moviesInfo }) => {
-    const location = useLocation();
+	const location = useLocation();
 
-    return <List>
-        {moviesInfo.map(({ id, title, release_date = '----', vote_average, poster_path }) => {
-            const fixedVotes = vote_average.toFixed(1);
-            const fixedDate = release_date.split('-').reverse().join('.');
+	return (
+		<List>
+			{moviesInfo.map(({ id, title, release_date, vote_average, poster_path }) => {
+				console.log('release_date: ', release_date);
+				const fixedVotes = vote_average.toFixed(1);
+				const fixedDate = release_date
+					? release_date.split('-').reverse().join('.')
+					: '';
+				const isReleaseDate = Boolean(fixedDate);
 
-            return <Item key={id}>
-                <NavLink to={`/movies/${id}`} state={{ from: location }}>
-                    <MoviePoster src={poster_path} alt={title} />
-                    <Wrapper>
-                        <Title>{title}</Title>
-                        <InfoWrapper>
-                            <Info>{fixedVotes}</Info>
-                            <Divider>I</Divider>
-                            <DateInfo>{fixedDate}</DateInfo>
-                        </InfoWrapper>
-                    </Wrapper>
-                </NavLink>
-            </Item>
-        })}
-    </List>;
+				return (
+					<Item key={id}>
+						<NavLink to={`/movies/${id}`} state={{ from: location }}>
+							<MoviePoster src={poster_path} alt={title} />
+							<Wrapper>
+								<Title>{title}</Title>
+								<InfoWrapper>
+									<Info>{fixedVotes}</Info>
+									{isReleaseDate && (
+										<>
+											<Divider>I</Divider>
+											<DateInfo>{fixedDate}</DateInfo>
+										</>
+									)}
+								</InfoWrapper>
+							</Wrapper>
+						</NavLink>
+					</Item>
+				);
+			})}
+		</List>
+	);
 };
 
 MoviesList.propTypes = {
-    moviesInfo: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        title: PropTypes.string.isRequired,
-        release_date: PropTypes.string,
-        vote_average: PropTypes.number.isRequired,
-        poster_path: PropTypes.any.isRequired
-    }),),
-}
+	moviesInfo: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.number.isRequired,
+			title: PropTypes.string.isRequired,
+			release_date: PropTypes.string,
+			vote_average: PropTypes.number.isRequired,
+			poster_path: PropTypes.any.isRequired,
+		})
+	),
+};
 
 export { MoviesList };
